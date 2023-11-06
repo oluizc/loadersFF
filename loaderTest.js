8e56862532f15d27d378188243875bb282605142
oluizc <150074900+oluizc@users.noreply.github.com>
2023-11-06 19:08:36 -0300

    Update loaderTest.js

--- loaderTest.js
+++ loaderTest.js
@@ -1,4 +1,4 @@
-// Create the overlay and SVG elements
+// Create the overlay and Lottie container
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
@@ -14,10 +14,9 @@ overlay.style.cssText = `
     z-index: 999;
 `;
 
-const svgImage = document.createElement('img');
-svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/preload.svg';
-svgImage.style.cssText = `
+const lottieContainer = document.createElement('div');
+lottieContainer.id = 'lottieContainer';
+lottieContainer.style.cssText = `
     max-width: 100%;
     max-height: 100%;
     display: none;
@@ -26,25 +25,29 @@ svgImage.style.cssText = `
     left: 50%;
     transform: translate(-50%, -50%);
 `;
-svgImage.style.display = 'none';
+lottieContainer.style.display = 'none';
 
 // Append the elements to the body
 document.body.appendChild(overlay);
-document.body.appendChild(svgImage);
+document.body.appendChild(lottieContainer);
 
-// Function to hide the overlay and display the SVG
+// Function to hide the overlay and display the Lottie animation
 function hideOverlay() {
     overlay.style.display = 'none';
-    svgImage.style.display = 'block';
+    lottieContainer.style.display = 'block';
 }
 
-// Add an event listener to hide the overlay when all external JS files are loaded
-document.addEventListener('DOMContentLoaded', () => {
-    // Replace the following lines with the actual code that loads your external JS files
-    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
-    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
-});
+// Load the Lottie animation
+const animationData = {
+    container: lottieContainer,
+    renderer: 'svg', // Use 'svg' or 'canvas' based on your preference
+    loop: true, // Set to true if you want the animation to loop
+    autoplay: true, // Set to true if you want the animation to start automatically
+    path: 'https://lottie.host/7f4a8672-71cd-4c9e-9d18-9358e0d24868/cKulgbyrhA.json', // Replace with the actual URL of your Lottie JSON file
+};
 
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
-window.addEventListener('load', hideOverlay);
+const anim = lottie.loadAnimation(animationData);
+
+// Replace the following lines with the actual code that loads your external resources
+// For demonstration purposes, we'll use a setTimeout to simulate loading external resources.
+setTimeout(hideOverlay, 100); // Replace with your actual loading code.
